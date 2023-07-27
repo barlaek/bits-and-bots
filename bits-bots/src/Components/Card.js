24f46159c7e5dde08f49af4a1fc3051c95428b11
Liridona <91525128+barlaek@users.noreply.github.com>
2023-07-27 19:28:27 +0200

    testing out cards

--- bits-bots/src/Components/Card.js
+++ bits-bots/src/Components/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import * as styled from "./Card.styles.js";
 
 function Card(props) {
   const content = props.data.map((prop) => {
     return (
-      <div key={prop.name}>
+      <styled.Card key={prop.name}>
+        <img src={prop.images[0]} alt="game image" />
+        {console.log(prop.images[0])}
         <h3 key={prop.name}>{prop.name}</h3>
-      </div>
+      </styled.Card>
     );
   });
   return <div>{content}</div>;
