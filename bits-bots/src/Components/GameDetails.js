673ee9517937a3547239eb544aee8e6ffe96d89a
Liridona <91525128+barlaek@users.noreply.github.com>
2023-07-27 21:51:43 +0200

    debugging the mapping function

--- bits-bots/src/Components/GameDetails.js
+++ bits-bots/src/Components/GameDetails.js
@@ -1,11 +1,19 @@
 import React from "react";
+import * as styled from "./GameDetails.styles.js";
 
 function GameDetails(props) {
     const details = props.data
+
+    // const gallery = details.images.map((image) => {
+    //     return (
+    //         <img src={image.src} alt={image.alt} />
+    //     )
+    // })
+
     return (
-        <div>
+        <styled.Container>
             <h2>{details.name}</h2>
-        </div>
+        </styled.Container>
     )
 }
 
