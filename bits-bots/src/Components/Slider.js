5681fe49f6410230158df9cc862d0b44e64f4a88
Liridona <91525128+barlaek@users.noreply.github.com>
2023-07-30 19:10:49 +0200

    debugging slider

--- bits-bots/src/Components/Slider.js
+++ bits-bots/src/Components/Slider.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import * as styled from "./Slider.styles.js";
 
-const Slider = (props) => {
+const Slider = ({bannerProduct}) => {
     const [imageIndex, setImageIndex] = useState(0);
 
     return (
         <styled.Container>
-            <styled.ImageContainer src={props.images} alt={props.images}/> 
+            <styled.ImageContainer src={bannerProduct.images[0].src} alt="whatever"/> 
         </styled.Container>
     )
 }
