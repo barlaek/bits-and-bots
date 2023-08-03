506c9864cb4cb9965a81c8ac82a3fd4f6203a471
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-08-03 10:39:37 +0200

    slider glider

--- bits-bots/src/Components/Slider.js
+++ bits-bots/src/Components/Slider.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { Children, useState } from "react";
 import * as styled from "./Slider.styles.js";
 
-const Slider = (bannerProduct) => {
-    // const hero = bannerProduct.map((images) => {
-    //     return images;
-    // })
+const Slider = (children) => {
+    const [position, setPosition] = useState(0);
+    const directions = {
+        NEXT: "next",
+        PREV: "previous",
+    };
 
-    const hero = bannerProduct.bannerProduct.map((images) => {
-        return <styled.ImageContainer key={images.name} src={images.src} alt={images.alt} />
+    const handlePreviousClick = () => {
+        handleArrowClick(directions.PREV);
+    };
+
+     const handleNextClick = () => {
+        handleArrowClick(directions.NEXT);
+    };
+
+     const handleArrowClick = (direction) => {
+        let newPosition = 0;
+        if (direction === directions.NEXT) {
+            newPosition = position === children.length - 1 ? 0 : position + 1;
+        } else {
+            newPosition = position === 0 ? children.length - 1 : position -1 ;
+        }
+
+        handleSlide(newPosition);
+    };
+
+    const handleSlide = (position) => {
+        setPosition(position);
+    };
+
+    const hero = children.bannerProduct.map((images) => {
+        return <styled.Image key={images.name} src={images.src} alt={images.alt} />
     })
 
     return (
-        <styled.Container key={hero.name}>
-            {hero}
+        <styled.Container hasMultipleImages={children.length > 1}>
+            <styled.CarouselImages position={position}>
+                {hero}
+            </styled.CarouselImages>
         </styled.Container>
     )
 }
--- bits-bots/src/Components/Slider.styles.js
+++ bits-bots/src/Components/Slider.styles.js
@@ -4,9 +4,45 @@ export const Container = styled.div`
     width: 350px;
     height: 150px;
     postion: relative;
+
+    ${({ hasMultipleImages }) => {
+        if (hasMultipleImages) {
+            return `
+                &:before {
+                    background-image: linear-gradient(to left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3));
+                    position: abosulte;
+                    left: 0;
+                    height: 100%;
+                    content: "";
+                    z-index: 2;
+                }
+
+                &:after {
+                    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3));
+                    position: absolute;
+                    right: 0;
+                    top: 0;
+                    height: 100%;
+                    width: 48px;
+                    content: "";
+                    z-index: 2;
+                }
+            `;
+        }
+        return null;
+    }};
 `;
 
-export const ImageContainer = styled.img`
+export const CarouselImages = styled.div.attrs({
+    style: ({ position }) => ({
+        transform: `translateX(${position * -100}%)`,
+    }),
+})`
+    display: flex;
+    transition: transform 0.45s ease;
+`;
+
+export const Image = styled.img`
     width: 100%;
     height: 100%;
     object-fit: cover;
