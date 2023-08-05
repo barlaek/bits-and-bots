e76d97e2378f826db43f6560c458a71bac0e9d60
Liridona <91525128+barlaek@users.noreply.github.com>
2023-08-05 13:46:18 +0200

    converts the string to a number to a decimal

--- bits-bots/src/Components/Card.js
+++ bits-bots/src/Components/Card.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import * as styled from './Card.styles.js';
 
 function Card(props) {
+
   const content = props.filteredProductList.map((prop) => {
     return (
       <styled.Card key={prop.name}>
         <styled.GameImage src={prop.images[0].src} alt={prop.images[0].alt} />
         <styled.Data>
           <h3 key={prop.name}>{prop.name}</h3>
-          <h3>${prop.prices.sale_price}</h3>
+          <h3>${parseInt((`${prop.prices.sale_price}`)/100).toFixed(2)}</h3>
           <Link to={`${prop.id}`} style={{ color: "#fff", hover: "cursor" }}>View details</Link>
         </styled.Data>
       </styled.Card>
--- bits-bots/src/Components/Slider.js
+++ bits-bots/src/Components/Slider.js
@@ -3,73 +3,18 @@ import { Link } from "react-router-dom";
 import chevronRight from "../images/chevron_right.svg";
 import chevronLeft from "../images/chevron_left.svg"
 import * as styled from "./Slider.styles.js";
-import Loading from "./Loading";
-import { useApi } from "../Utilities/Hooks/useApi";
-import { productUrl } from "../Utilities/Constants/endpoints";
-
-// function Slider() {
-//     const { data, loading, error } = useApi(`${productUrl}`);
-//     const [ imageIndex, setImageIndex] = useState(0);
-
-//     const isOnSale = data.map((product) => {
-//         if(product.on_sale === true) {
-//             return product.images[0];
-//         } else if(!product) {
-//             return <div key={error}>Error</div>
-//         } else {
-//             return <Loading key={loading} />
-//         }
-//     }) 
-
-//     console.log(isOnSale);
-
-//     const next = () => {
-//         setImageIndex((state) => (state += 1));
-//         if(imageIndex === data.length -1){
-//             setImageIndex(0);
-//         };
-//     };
-
-//     const previous = () => {
-//         setImageIndex((state => state -= 1));
-//         if(imageIndex === 0) {
-//             setImageIndex(0);
-//         };
-//     };
-//     return (
-//         <styled.Container>
-//            <Link to="">
-//                  <styled.Title>Titles on sale: </styled.Title>
-//                  <styled.Image src="" alt="" />
-//                  <styled.NavButton right onClick={next}>
-//                      <img src={chevronRight} alt="right arrow"/>
-//                  </styled.NavButton>
-//                  <styled.NavButton onClick={previous}>
-//                      <img src={chevronLeft} alt="left arrow"/>
-//                  </styled.NavButton>
-//              </Link>
-//         </styled.Container>
-//     )
-// }
-
-// export default Slider;
 
 const Slider = (children) => {
     const [imageIndex, setImageIndex] = useState(0);
     let data = children.productOnSale.map((product) => {
         if(product.on_sale === true) {
           return (
-                // <styled.Image src={product.images[imageIndex].src} alt={product.images[imageIndex].src} />
                 <styled.Image src={product.images[0].src} alt={product.images[0].src} />
           )
         }
       })
 
     let id = children.productOnSale.map((id) => {
-        // return {
-        //     id: id.id,
-        //     name: id.name,
-        // }
         return(
             <styled.Title>Titles on sale: {id.name}</styled.Title>
         )
