15bb2addcf4de5a4a47ff0e206db29dccdef417e
Liridona <91525128+barlaek@users.noreply.github.com>
2023-08-08 20:48:11 +0200

    debugging conditional rendering of log btn

--- bits-bots/src/Components/ProfileSelect.js
+++ bits-bots/src/Components/ProfileSelect.js
@@ -1,35 +1,61 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import * as styled from './ProfileSelect.styles.js';
 import { useNavigate } from 'react-router-dom';
 import Logout from './Logout.js';
+import { UserContext } from '../Utilities/Context/UserContext.js';
+import useStorage from '../Utilities/Hooks/useStorage.js';
 
 const ProfileSelect = () => {
-  const [ user, setUser ] = useState(false);
+  // const { currentUser, setCurrentUser } = useContext(UserContext)
+  // const { userBody } = useStorage();
   const landing = useNavigate();
 
   const handleClick = () => {
     landing('/landing');
   };
 
-  const checkAuth = () => {
-    if(localStorage.length === 0) {
-      setUser(user, false);
-    } else {
-      setUser(user, true);
-    }
-  }
+  // const isLoggedin = setCurrentUser(userBody);
+  // let button;
 
-  console.log(user);
+  // if(isLoggedin) {
+  //   button = <Logout />
+  // } else {
+  //   button = <styled.Login onClick={handleClick}>Login</styled.Login>
+  // }
+
+
+  // const checkAuth = () => {
+  //   if(!currentUser) {
+  //     return <styled.Login>Login</styled.Login>
+  //   } else {
+
+  //   }
+  // }
+
+  // if(!currentUser) {
+  //   return <styled.Login onClick={handleClick}>Login</styled.Login>
+  // } else {
+  //   return <Logout />
+  // }
+  // const checkAuth = () => {
+  //   if(currentUser === null) {
+  //     if(!userBody) {
+  //       return <styled.Login>Login</styled.Login>
+  //     }
+  //   }
+  //   return <Logout />
+  // }
 
 
   return (
-    <styled.Container onChange={checkAuth}>
+    <styled.Container>
       {/* {user ? <Logout /> : <styled.Login onClick={handleClick}>Login</styled.Login>} */}
-      {!user ? (
+      {/* {!user ? (
         <styled.Login onClick={handleClick}>Login</styled.Login>
       ) : (
         <Logout />
-      )}
+      )} */}
+      {/* {!currentUser ? <styled.Login onClick={handleClick}>Login</styled.Login> : <Logout />} */}
     </styled.Container>
   );
 };
