1f114e0674110669819b3e29755c5b6103eeb8fd
Liridona <91525128+barlaek@users.noreply.github.com>
2023-08-13 18:51:31 +0200

    render tests

---
+++ bits-bots/src/Tests/Error.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react"
+import Error from "../Components/Error"
+
+test("error component renders correctly", () => {
+    render(
+        <Error />
+    );
+})
--- bits-bots/src/Tests/GameDetails.test.js
+++ bits-bots/src/Tests/GameDetails.test.js
@@ -1,18 +1,11 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import GameDetails from '../Components/GameDetails';
 
 test("renders gamedetails component", () => {
-    const details = [{
-        id: 200,
-        name: "string",
+    const data = {
         description: "string",
-        images: [{
-            id: 201,
-            name: "string",
-            src: "url",
-        }]
-    }]
+    };
   render(
-        <GameDetails details={details} />
+        <GameDetails data={data} />
   );
 })
