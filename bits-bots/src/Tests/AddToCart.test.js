5948ddf13214c86fac7f218d53f9f82616476dc5
Liridona <91525128+barlaek@users.noreply.github.com>
2023-08-13 15:05:37 +0200

    debugging toHaveBeenCalled function

---
+++ bits-bots/src/Tests/AddToCart.test.js
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import AddToCart from "../Components/AddToCart";
+
+describe("AddToCart", () => {
+    test("component renders correctly", () => {
+        const mockCallBack = jest.fn();
+        const data = {
+            name: "name",
+            prices: {
+                price: "price",
+            },
+        };
+        render(
+            <MemoryRouter>
+                <AddToCart 
+                    data={data}
+                    onClick={mockCallBack}
+                />
+            </MemoryRouter>
+        );
+        // const onAdd = screen.getByRole("button");
+        // fireEvent.click(onAdd);
+        expect(mockCallBack).toHaveBeenCalledTimes(1);
+    })
+})
