10858e9a445866bc3685c72e279cfe75d4da0806
Liridona <91525128+barlaek@users.noreply.github.com>
2023-08-13 13:51:31 +0200

    card test

---
+++ bits-bots/src/Tests/Card.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import Card from '../Components/Card';
+import { MemoryRouter } from 'react-router-dom';
+
+test("renders card component", () => {
+  const filteredProductList = [{
+    id: 200,
+    name: "name",
+    images: [
+      {
+        alt: "alt",
+      }
+    ],
+    prices: {
+      sales_price: "number"
+    },
+  },
+];
+
+  render(<MemoryRouter><Card filteredProductList={filteredProductList} /></MemoryRouter>)
+})
--- bits-bots/src/Tests/Login.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from '../Components/Login';
-
-describe('Login', () => {
-  test('displays a login form', () => {
-    render(<Login />);
-    screen.debug();
-  });
-});
